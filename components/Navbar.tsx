c66cef091652e05872afd25b93f73892ccedf3de
m0onpi <michael.pigottbiz@gmail.com>
2024-02-24 18:30:32 +0000

    newsletter

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,10 +38,10 @@ export default function Navbar() {
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link to="home">
-              <div className="container flex items-center space-x-2">
+            </Link>
+            <div className="container flex items-center space-x-2">
                 <h2 className="text-2xl font-bold">Michael Pigott</h2>
               </div>
-            </Link>
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
